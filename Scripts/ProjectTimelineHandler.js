49bd4b3a003ee7486912ebb1aacdc0d547eef83f
Valente-Coding <goncalo.p.valente@hotmail.com>
2025-10-14 00:02:43 +0100

    Add software logos and improve project timeline UI
    Added Unity and Unreal logo images and updated project data to include software logos. Refactored project tags and timeline card rendering for better visual grouping. Enhanced CSS for improved layout, tag styling, and software logo display. Updated index.html to reflect new UI elements and added instructions for resume image interaction.

--- Scripts/ProjectTimelineHandler.js
+++ Scripts/ProjectTimelineHandler.js
@@ -3,7 +3,8 @@ var Projects = [
     {
         "ProjectName": "Venci's Adventures",
         "ProjectDescription": "Dive into the world of Vencis and tackle cybersecurity challenges where mastering complex topics becomes effortlessly engaging. Let's game, learn, and secure!",
-        "ProjectTags": ["Singleplayer", "eLearning", "Unity", "Internship"],
+        "ProjectTags": ["Singleplayer", "eLearning", "Internship"],
+        "ProjectSoftwareLogos": ["Unity"],
         "ProjectImage": "Images/VencisAdventures.png",
         "ProjectVideo": "Images/VencisAdventures.mp4",
         "ProjectVideoLink": "https://www.linkedin.com/posts/emvenci_lets-game-learn-and-secure-activity-7117422051514023936-N7AB",
@@ -12,7 +13,8 @@ var Projects = [
     {
         "ProjectName": "Mar Made Sushi",
         "ProjectDescription": "Mar Made Sushi is a fun multiplayer prototype where players must work together to manage a restaurant. Collaborate with your teammates to fish for ingredients, cook delicious dishes, and host clients efficiently. Teamwork and coordination are key to keeping the restaurant running smoothly and satisfying your customers.",
-        "ProjectTags": ["Multiplayer", "Strategy", "Unity", "University Project"],
+        "ProjectTags": ["University Project", "Online Multiplayer", "Strategy"],
+        "ProjectSoftwareLogos": ["Unity"],
         "ProjectImage": "Images/MarMadeSushi.png",
         "ProjectVideo": "Images/MarMadeSushi.mp4",
         "ProjectVideoLink": "https://youtu.be/4pTJCFrdATc",
@@ -21,7 +23,8 @@ var Projects = [
     {
         "ProjectName": "Deep Anomaly",
         "ProjectDescription": "Deep Anomaly is a local multiplayer party game where players must compete to catch the most fish avoiding the ones with anomalies.",
-        "ProjectTags": ["Local Multiplayer", "Party", "Unity", "Game Jam"],
+        "ProjectTags": ["Game Jam", "Local Multiplayer", "Party"],
+        "ProjectSoftwareLogos": ["Unity"],
         "ProjectImage": "Images/DeepAnomaly.png",
         "ProjectVideo": "Images/DeepAnomaly.mp4",
         "ProjectVideoLink": "",
@@ -30,7 +33,8 @@ var Projects = [
     {
         "ProjectName": "Mix & Serve",
         "ProjectDescription": "In Mix & Serve you impersonate a bartender in a busy bar. You must quickly mix and serve drinks to your customers while managing your time and resources effectively. Your goal is to have enough money to pay your rent at the end of the day.",
-        "ProjectTags": ["Singleplayer", "Simulation", "Unity", "Game Jam"],
+        "ProjectTags": ["Game Jam","Singleplayer", "Simulation"],
+        "ProjectSoftwareLogos": ["Unity"],
         "ProjectImage": "Images/MixAndServe.png",
         "ProjectVideo": "Images/MixAndServeFast.mp4",
         "ProjectVideoLink": "",
@@ -39,7 +43,8 @@ var Projects = [
     {
         "ProjectName": "Comedy Showdown",
         "ProjectDescription": "Your goal is to write your own jokes to different people based on their likings. Each person has a different sense of humor, so you must choose the right joke to make them laugh. If you tell a joke that they don't like, they will get angry. If you fail at least 3 times, you lose the game.",
-        "ProjectTags": ["Singleplayer", "Comedy", "Unity", "Game Jam"],
+        "ProjectTags": ["Game Jam", "Singleplayer", "Comedy"],
+        "ProjectSoftwareLogos": ["Unity"],
         "ProjectImage": "Images/ComedyShowdown.png",
         "ProjectVideo": "Images/ComedyShowdown.mp4",
         "ProjectVideoLink": "",
@@ -48,7 +53,8 @@ var Projects = [
     {
         "ProjectName": "Sudoku 48H",
         "ProjectDescription": "Sudoku48H is a project developed as part of a 48-hour challenge to test the developer's skills. The objective was to create a functional Sudoku game in Unity without referencing online resources. The project was completed in under 14 hours, with a significant portion of time spent refining the UI rather than the core game logic.",
-        "ProjectTags": ["Singleplayer", "Strategy", "Unity", "Challenge"],
+        "ProjectTags": ["Challenge", "Singleplayer", "Strategy" ],
+        "ProjectSoftwareLogos": ["Unity"],
         "ProjectImage": "Images/Sudoku48h.png",
         "ProjectVideo": "Images/Placeholder.mp4",
         "ProjectVideoLink": "",
@@ -71,10 +77,12 @@ function LoadProjects() {
             '</div>'+
 
             '<div class="ProjectTimelineCardInfo">'+
-                '<div class="ProjectTimelineImage" style="background-image: url(\'' + l_project.ProjectImage + '\');"></div>'+
+                '<div class="ProjectTimelineImage" style="background-image: url(\'' + l_project.ProjectImage + '\');">' +
+                    '<div class="ProjectTimelineSoftwareLogos">' + l_project.ProjectSoftwareLogos.map(logo => '<div class="SoftwareLogoContainer"><div class="SoftwareLogo" style="background-image: url(Images/' + logo + '.png);"></div></div>').join('') + '</div>'+
+                '</div>'+
                 '<div class="ProjectTimelineInfoContainer">'+
                     '<div class="ProjectTimelineTitle bebas-neue-regular">' + l_project.ProjectName + '</div>'+
-                    '<div class="ProjectTimelineGameTags martel-sans-bold">' + l_project.ProjectTags.join('</br>') + '</div>'+
+                    '<div class="ProjectTimelineGameTags martel-sans-bold"><span>' + l_project.ProjectTags.join('</span><span>') + '</span></div>'+
                 '</div>'+
             '</div>'+
         '</div>';
